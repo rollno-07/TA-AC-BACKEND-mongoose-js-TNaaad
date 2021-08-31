eec43ad961e5801831b41780fa4161c7798aa707
Vivek Rawat <vivekrawat0107@gmail.com>
2021-08-31 16:11:58 +0530

    handson5_schemaTypes_mongoose_model

---
+++ block-BNaacm/server/models/users.js
@@ -0,0 +1,24 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var users=mongoose.model('User',user)
+
+var user=new Schema({
+    name:String,
+    email:{type:String,lowercase:true},
+    age:{type:Number,default:0},
+    favorites:[String],
+    marks:[Number]
+
+})
+
+
+var address= new Schema({
+    village:String,
+    city:String,
+    state:String,
+    pin:Number,
+    user:Schema.Types.ObjectId
+})
+
+module.exports=users
